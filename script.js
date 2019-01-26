dfb6646c269041bf96ce0e744fb971b748c77b3c
NikolaevaO <oksana.bazyk@gmail.com>
2019-01-26 20:29:45 +0300

    Добавлена анимация логотипа и мобильного меню

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ var menuVisible = false;
 
 function showHideMenu() {
   if (menuVisible) {
-    document.getElementById("mobileMenu").style.display = "none";
+    document.getElementById("mobileMenu").style.opacity = 0;
     menuVisible = false;
   } else {
-    document.getElementById("mobileMenu").style.display = "block";
+    document.getElementById("mobileMenu").style.opacity = 1;
     menuVisible = true;
   }
 }
